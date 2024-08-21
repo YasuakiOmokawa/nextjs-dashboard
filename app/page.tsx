8963dc8a0a5e74ff5adcdfd3371dc14bfe3c5e3b
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-21 11:08:14 +0900

    add image for desktop and mobile

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import { bungee_spice } from '@/app/ui/fonts';
+import Image from 'next/image'
 
 export default function Page() {
   return (
@@ -30,6 +31,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src='/hero-desktop.png'
+            width={1000}
+            height={760}
+            className='hidden md:block'
+            alt='Screenshots of the dashboard project showing desktop version'
+          />
+          <Image
+            src='/hero-mobile.png'
+            width={560}
+            height={620}
+            className='block md:hidden'
+            alt='Screenshots of the dashborad project showing mobile version'
+          />
         </div>
       </div>
     </main>
