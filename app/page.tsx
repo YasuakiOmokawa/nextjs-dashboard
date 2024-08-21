9df0ce82c2570639318660cc08310a0dc81677b3
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-21 10:16:13 +0900

    add Bungee_Spice font

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css'
 import { lusitana } from './ui/font';
+import { bungee_spice } from './ui/font';
 
 export default function Page() {
   return (
@@ -14,7 +15,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className={styles.shape} />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong className={bungee_spice.className}>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
--- app/ui/font.ts
+++ app/ui/font.ts
@@ -1,5 +1,9 @@
 import { Inter } from "next/font/google";
 import { Lusitana } from "next/font/google";
+import { Bungee_Spice } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
+
 export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
+
+export const bungee_spice = Bungee_Spice({ subsets: ["latin"], weight: "400" });
