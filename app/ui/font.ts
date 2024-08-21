fa5b4794186fcd334da30687d1c17cbb6cfa771b
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-21 10:17:53 +0900

    add Lusitana font weight 700

--- app/ui/font.ts
+++ app/ui/font.ts
@@ -4,6 +4,9 @@ import { Bungee_Spice } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 
-export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export const bungee_spice = Bungee_Spice({ subsets: ["latin"], weight: "400" });
