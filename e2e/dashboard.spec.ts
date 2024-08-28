b42484921b4f3442e518ed55d290ac98bf3e1edd
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-28 13:35:58 +0900

    impl dashboard.spec.ts

---
+++ e2e/dashboard.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("dashboard", () => {
+  test("has heading", async ({ page }) => {
+    await page.goto("http://localhost:3000/dashboard");
+
+    await expect(page.getByRole("heading", { level: 1 })).toHaveText(
+      "Dashboard"
+    );
+  });
+});
