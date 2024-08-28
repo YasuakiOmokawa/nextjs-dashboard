87cd94b3bb26091cc6fd4df0371f93528082ad29
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-28 14:11:57 +0900

    todo

--- e2e/dashboard.spec.ts
+++ e2e/dashboard.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("app/dashboard/page.tsx", () => {
   test.beforeEach(async ({ page }) => {
+    // TODO: 環境変数ファイルにlocalhost:3000の部分を分離する
     await page.goto("http://localhost:3000/dashboard");
   });
 
