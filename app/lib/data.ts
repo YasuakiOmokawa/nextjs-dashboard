d33ea48d2355c75b8fb3edf6e6a010c9614a20e9
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-30 15:57:52 +0900

    remove fetch revenue method with vercel sdk

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -5,13 +5,12 @@ import {
   InvoiceForm,
   InvoicesTable,
   LatestInvoiceRaw,
-  Revenue,
 } from "./definitions";
 import { formatCurrency } from "./utils";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export async function fetchRevenuePrisma() {
+export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
@@ -24,25 +23,6 @@ export async function fetchRevenuePrisma() {
     // console.log('Data fetch completed after 3 seconds.');
 
     return data;
-  } catch (error) {
-    console.error("Database Error:", error);
-    throw new Error("Failed to fetch revenue data.");
-  }
-}
-
-export async function fetchRevenue() {
-  try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data.rows;
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch revenue data.");
