e7366b34cf2fc405fee4c82f33478f622869ac64
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-09-06 13:53:38 +0900

    create seedFile flag context block

--- __tests__/lib/seedCreator.test.ts
+++ __tests__/lib/seedCreator.test.ts
@@ -1,19 +1,34 @@
 import { readAllTables } from "@/app/lib/seeds/seedCreator";
 
-vi.mock("@/app/lib/seeds/createSeedFile", () => {
-  return {
-    createSeedFile: vi
-      .fn()
-      .mockImplementation(() => console.log("mocked createSeedFile")),
-  };
-});
 console.log = vi.fn();
 
 describe("readAllTables", () => {
-  test("calls createSeedFile() when seedFile: true", async () => {
-    await readAllTables({ seedFile: true, logTables: false });
+  describe("seedFile flag", () => {
+    beforeAll(() => {
+      vi.mock("@/app/lib/seeds/createSeedFile", () => {
+        return {
+          createSeedFile: vi
+            .fn()
+            .mockImplementation(() => console.log("mocked createSeedFile")),
+        };
+      });
+    });
 
-    expect(console.log).toHaveBeenNthCalledWith(1, "mocked createSeedFile");
-    expect(console.log).toHaveBeenNthCalledWith(2, "Done ---");
+    afterEach(() => {
+      console.log = vi.fn(); // reset console
+    });
+
+    test("calls createSeedFile() when flag is true", async () => {
+      await readAllTables({ seedFile: true, logTables: false });
+
+      expect(console.log).toHaveBeenNthCalledWith(1, "mocked createSeedFile");
+      expect(console.log).toHaveBeenNthCalledWith(2, "Done ---");
+    });
+
+    test("do not call createSeedFile() when flag is false", async () => {
+      await readAllTables({ seedFile: false, logTables: false });
+
+      expect(console.log).toHaveBeenNthCalledWith(1, "Done ---");
+    });
   });
 });
