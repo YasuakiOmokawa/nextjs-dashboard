59e586fe74055873036b1f537bfed4fc569af657
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-09-06 17:01:44 +0900

    add tests on a flag named allSeeds

--- __tests__/lib/seedCreator.test.ts
+++ __tests__/lib/seedCreator.test.ts
@@ -1,8 +1,46 @@
 import { readAllTables } from "@/app/lib/seeds/seedCreator";
 
-console.log = vi.fn();
+console.log = vi.fn(); // mock console
 
 describe("readAllTables", () => {
+  describe("allSeeds flag", () => {
+    beforeAll(() => {
+      vi.mock("@/app/lib/seeds/findAndRefind", () => {
+        return {
+          findAndRefind: vi.fn().mockReturnValue("mocked findAndRefind"),
+        };
+      });
+    });
+
+    afterEach(() => {
+      console.log = vi.fn(); // reset console
+    });
+
+    test("calls findAndRefind() when flag is true", async () => {
+      await readAllTables({ allSeeds: true, logTables: true });
+
+      expect(console.log).toHaveBeenNthCalledWith(
+        1,
+        "Start -->",
+        "mocked findAndRefind",
+        "<-- End"
+      );
+      expect(console.log).toHaveBeenNthCalledWith(2, "Done ---");
+    });
+
+    test("do not call findAndRefind() when flag is false", async () => {
+      await readAllTables({ allSeeds: false, logTables: true });
+
+      expect(console.log).toHaveBeenNthCalledWith(
+        1,
+        "Start -->",
+        {},
+        "<-- End"
+      );
+      expect(console.log).toHaveBeenNthCalledWith(2, "Done ---");
+    });
+  });
+
   describe("seedFile flag", () => {
     beforeAll(() => {
       vi.mock("@/app/lib/seeds/createSeedFile", () => {
