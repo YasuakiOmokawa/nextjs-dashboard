a85ce299707d015e40ee1f6d5c6a85a2c5459c58
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-09-17 13:34:49 +0900

    enable ppr on dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 import React from "react";
 
+export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
