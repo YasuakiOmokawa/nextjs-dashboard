ed565d5fea9bb8a3585b277a618c0eb59d82ce4e
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-10-06 12:01:20 +0900

    extract Props

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,16 +1,15 @@
-import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
-import { fetchFilteredInvoices } from '@/app/lib/data';
+import Image from "next/image";
+import { UpdateInvoice, DeleteInvoice } from "@/app/ui/invoices/buttons";
+import InvoiceStatus from "@/app/ui/invoices/status";
+import { formatDateToLocal, formatCurrency } from "@/app/lib/utils";
+import { fetchFilteredInvoices } from "@/app/lib/data";
 
-export default async function InvoicesTable({
-  query,
-  currentPage,
-}: {
+type Props = {
   query: string;
   currentPage: number;
-}) {
+};
+
+export default async function InvoicesTable({ query, currentPage }: Props) {
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
