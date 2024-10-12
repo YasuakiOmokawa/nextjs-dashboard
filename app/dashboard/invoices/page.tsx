e3177ea8b17650bfcca0fa9e1dcf1506ce919c2e
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-10-12 15:06:30 +0900

    assign totalPages to component for pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,7 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
+import { fetchInvoicesPages } from "@/app/lib/data";
 
 export default async function Page({
   searchParams,
@@ -15,6 +16,7 @@ export default async function Page({
     query: searchParams?.query || "",
     currentPage: Number(searchParams?.page) || 1,
   };
+  const totalPages = await fetchInvoicesPages(tableProps.query);
 
   return (
     <div className="w-full">
@@ -32,7 +34,7 @@ export default async function Page({
         <Table {...tableProps} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
