4d9349315e96013755f48a319eb738fba0081e80
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-11-02 12:06:09 +0900

    change

---
+++ prisma/migrations/20241102030559_remove_updated_at_on_tag2/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updated_at` on the `tags2` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tags2" DROP COLUMN "updated_at";
