57c560e256cca2380e27b41a25e2c8aff281cf22
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-11-02 11:40:21 +0900

    changes

---
+++ prisma/migrations/20241102024008_remove_timestamp_on_tag2/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `created_at` on the `tags2` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `tags2` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tags2" DROP COLUMN "created_at",
+DROP COLUMN "updated_at";
