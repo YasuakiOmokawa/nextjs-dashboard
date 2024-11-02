f94a7ff75d01da12be5f84a7f0557d85626873a5
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-11-02 12:17:37 +0900

    changes

---
+++ prisma/migrations/20241102031729_remove_created_at_on_tag2/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `created_at` on the `tags2` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tags2" DROP COLUMN "created_at";
