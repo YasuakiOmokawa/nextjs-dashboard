be8ec75d6cc831d37c1351a312594d85fb7211b5
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-11-10 15:44:05 +0900

    rename chain > stackMiddleware

--- middlewares/stackMiddleware.ts
+++ middlewares/stackMiddleware.ts
@@ -1,15 +1,14 @@
 import { NextMiddleware, NextResponse } from "next/server";
+import { MiddlewareFactory } from "./types";
 
-type MiddlewareFactory = (middleware: NextMiddleware) => NextMiddleware;
-
-export function chain(
-  functions: MiddlewareFactory[],
+export function stackMiddleware(
+  functions: MiddlewareFactory[] = [],
   index = 0
 ): NextMiddleware {
   const current = functions[index];
 
   if (current) {
-    const next = chain(functions, index + 1);
+    const next = stackMiddleware(functions, index + 1);
     return current(next);
   }
 
