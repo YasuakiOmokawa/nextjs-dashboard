cbf6c2d4ff4ca34c52ab4f232f3b13a28fc561b2
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-11-10 15:47:53 +0900

    conditionally add certain middlewares in certain routes

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,36 @@
-import { chain } from "./middlewares/chain";
+import { NextFetchEvent, NextResponse, type NextRequest } from "next/server";
+import { stackMiddleware } from "./middlewares/stackMiddleware";
 import { redirectHoge } from "./middlewares/redirectHoge";
+import { MiddlewareFactory } from "./middlewares/types";
 
-export default chain([redirectHoge]);
+// mapping route and middleware
+const routeMiddlewares: { [key: string]: MiddlewareFactory[] } = {
+  "/hoge": [redirectHoge],
+};
+
+export async function middleware(request: NextRequest, event: NextFetchEvent) {
+  const response = NextResponse.next();
+  const path = request.nextUrl.pathname;
+
+  for (const route in routeMiddlewares) {
+    if (path.startsWith(route)) {
+      const middlewares = routeMiddlewares[route];
+      const stackedMiddleware = stackMiddleware(middlewares);
+      return stackedMiddleware(request, event);
+    }
+  }
+  return response;
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
