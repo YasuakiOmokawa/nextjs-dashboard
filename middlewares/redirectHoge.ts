8908748c0ad4cbefa772bd1a39d434c480358bee
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-11-10 14:39:10 +0900

    impl splited middleware function

---
+++ middlewares/redirectHoge.ts
@@ -0,0 +1,17 @@
+import { resolveTxt } from "dns/promises";
+import {
+  NextMiddleware,
+  NextRequest,
+  NextResponse,
+  NextFetchEvent,
+} from "next/server";
+
+export function redirectHoge(middleware: NextMiddleware) {
+  return async (request: NextRequest, event: NextFetchEvent) => {
+    if (request.nextUrl.pathname.startsWith("/hoge")) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+
+    return middleware(request, event);
+  };
+}
