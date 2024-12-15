2031d3ad851e588e30ff80d9d4be9f0c076342a0
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-12-15 17:00:50 +0900

    add rest of form validation error

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -71,6 +71,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount &&
+              state.errors.amount.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
 
         {/* Invoice Status */}
@@ -111,6 +119,27 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 </label>
               </div>
             </div>
+          </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status &&
+              state.errors.status.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+          {/* message if Error */}
+          <div
+            id="error-message"
+            aria-live="polite"
+            aria-atomic="true"
+            className="mt-4"
+          >
+            {state.message && (
+              <p className="mt-2 text-sm text-red-500" key={state.message}>
+                {state.message}
+              </p>
+            )}
           </div>
         </fieldset>
       </div>
