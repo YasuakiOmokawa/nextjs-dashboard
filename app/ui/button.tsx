02eacbcea72c0f52d8783df608dd4acf0c5c8499
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-12-15 17:40:48 +0900

    disalbe submit button in sending data

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,19 +1,22 @@
-import clsx from 'clsx';
+import clsx from "clsx";
+import { useFormStatus } from "react-dom";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
 export function Button({ children, className, ...rest }: ButtonProps) {
+  const status = useFormStatus();
   return (
     <button
       {...rest}
       className={clsx(
-        'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
-        className,
+        "flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
+        className
       )}
+      disabled={status.pending}
     >
-      {children}
+      {status.pending ? "Sending..." : children}
     </button>
   );
 }
