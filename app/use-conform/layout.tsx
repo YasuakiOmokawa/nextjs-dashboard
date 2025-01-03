3440a1d84ace3d7cf7bc42cf5a85772bcf812f94
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-01-03 18:18:36 +0900

    impl layout to provide context

---
+++ app/use-conform/layout.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+import FormProvider from "../ui/use-conform/form-provider";
+
+export default async function Layout({ children }: { children: ReactNode }) {
+  return <FormProvider>{children}</FormProvider>;
+}
