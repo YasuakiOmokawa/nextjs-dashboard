829ccc7473d33aaf96e5ddf8d14f806e2fd896ab
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-01-04 18:37:34 +0900

    add message field

--- app/ui/use-conform/form.tsx
+++ app/ui/use-conform/form.tsx
@@ -6,20 +6,36 @@ import { Button } from "../button";
 export default function Form() {
   const form = useFormMetadata();
   const [email] = useField<string>("email");
+  const [message] = useField<string>("message");
 
   return (
     <form id={form.id} onSubmit={form.onSubmit} noValidate>
+      <input type="hidden" name="submitType" value="confirm" />
       <div>
-        <label>Email</label>
-        <input
-          type="email"
-          key={email.key}
-          name={email.name}
-          defaultValue={email.initialValue}
-        />
-        <div className="text-red-500">{email.errors}</div>
+        <fieldset>
+          <label>Email</label>
+          <input
+            type="email"
+            key={email.key}
+            name={email.name}
+            defaultValue={email.initialValue}
+          />
+          <div className="text-red-500">{email.errors}</div>
+        </fieldset>
+        <fieldset className="mt-2">
+          <label>Message</label>
+          <input
+            type="text"
+            key={message.key}
+            name={message.name}
+            defaultValue={message.initialValue}
+          />
+          <div className="text-red-500">{message.errors}</div>
+        </fieldset>
       </div>
-      <Button type="submit">Go to Confirm</Button>
+      <div className="mt-6 flex">
+        <Button type="submit">Confirm</Button>
+      </div>
     </form>
   );
 }
