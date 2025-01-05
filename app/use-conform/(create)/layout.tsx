946734d80b7fc5ed4aa025e0663aca63a2818244
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-01-05 14:11:09 +0900

    move page to create context folder

--- app/use-conform/(create)/layout.tsx
+++ app/use-conform/(create)/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import FormProvider from "../ui/use-conform/form-provider";
+import FormProvider from "../../ui/use-conform/form-provider";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   return <FormProvider>{children}</FormProvider>;
--- app/use-conform/(create)/page.tsx
+++ app/use-conform/(create)/page.tsx
@@ -1,4 +1,4 @@
-import Form from "../ui/use-conform/form";
+import Form from "@/app/ui/use-conform/form";
 
 export default async function Page() {
   return (
