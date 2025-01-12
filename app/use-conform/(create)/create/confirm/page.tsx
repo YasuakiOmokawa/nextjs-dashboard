0ff451012f7319814c4f1c81c5290c74ab648931
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-01-12 12:19:45 +0900

    redirect if not from create form

--- app/use-conform/(create)/create/confirm/page.tsx
+++ app/use-conform/(create)/create/confirm/page.tsx
@@ -1,6 +1,17 @@
 import Form from "@/app/ui/use-conform/create/confirm/form";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
+  const headerList = await headers();
+
+  if (
+    headerList.get("referer") !==
+    `${process.env.PROTOCOL}${headerList.get("host")}/use-conform/create`
+  ) {
+    redirect("/use-conform/create");
+  }
+
   return (
     <main>
       <Form />
