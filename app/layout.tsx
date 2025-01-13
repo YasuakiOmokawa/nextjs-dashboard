fa0db31f2a5e732f833d85a9f5dd9d22cfdbed1e
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-01-13 15:07:22 +0900

    apply provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 import { Toaster } from "@/components/ui/sonner";
+import { Providers } from "./providers";
 
 export default function RootLayout({
   children,
@@ -10,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        {children}
+        <Providers>{children}</Providers>
         <Toaster />
       </body>
     </html>
