2f22a91999f02c28ab96b8bcb83706776adbf53b
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-01-25 14:26:57 +0900

    reset storage atom on reach thanks page

--- app/thanks/page.tsx
+++ app/thanks/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { emailAtom, nameAtom } from "@/app/lib/atoms/atoms";
+import { useSetAtom } from "jotai/react";
+import { RESET } from "jotai/utils";
+
 export default function Page() {
+  useSetAtom(emailAtom)(RESET);
+  useSetAtom(nameAtom)(RESET);
+
   return <p>thanks!</p>;
 }
