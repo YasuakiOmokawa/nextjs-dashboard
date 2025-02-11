f98c89224fdb1830bd838f1fffb4bed13d29128c
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-02-11 22:37:03 +0900

    add login atoms

--- app/lib/atoms/atoms.tsx
+++ app/lib/atoms/atoms.tsx
@@ -7,3 +7,21 @@ export const emailAtom = atomWithStorage<string>("emailAtom", "", undefined, {
 export const nameAtom = atomWithStorage<string>("nameAtom", "", undefined, {
   getOnInit: true,
 });
+
+export const loginEmailAtom = atomWithStorage<string>(
+  "loginEmailAtom",
+  "",
+  undefined,
+  {
+    getOnInit: true,
+  }
+);
+
+export const loginPasswordAtom = atomWithStorage<string>(
+  "loginPasswordAtom",
+  "",
+  undefined,
+  {
+    getOnInit: true,
+  }
+);
