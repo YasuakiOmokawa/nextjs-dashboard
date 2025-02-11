f6d4ec7f78c03eb513605948eb210136b75942d4
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-02-11 20:19:55 +0900

    use conform on form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -11,15 +11,32 @@ import { Button } from "./button";
 import { useActionState } from "react";
 import { authenticate } from "../lib/actions";
 import { useRedirectPath } from "../lib/hooks/login/useRedirectPath";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { loginSchema } from "../lib/schema/login/schema";
 
 export default function LoginForm() {
-  const [errorMessage, formAction] = useActionState(
+  const [errorMessage, action] = useActionState(
     authenticate.bind(null, useRedirectPath()),
     undefined
   );
+  const [form, fields] = useForm({
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: loginSchema });
+    },
+
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
 
   return (
-    <form action={formAction} className="space-y-3">
+    <form
+      id={form.id}
+      onSubmit={form.onSubmit}
+      action={action}
+      noValidate
+      className="space-y-3"
+    >
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -35,14 +52,15 @@ export default function LoginForm() {
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
+                key={fields.email.key}
                 type="email"
-                name="email"
+                name={fields.email.name}
                 placeholder="Enter your email address"
-                required
+                defaultValue={fields.email.value}
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            <div className="text-red-500">{fields.email.errors}</div>
           </div>
           <div className="mt-4">
             <label
@@ -54,15 +72,15 @@ export default function LoginForm() {
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
+                key={fields.password.key}
                 type="password"
-                name="password"
+                name={fields.password.name}
                 placeholder="Enter password"
-                required
-                minLength={6}
+                defaultValue={fields.password.value}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            <div className="text-red-500">{fields.password.errors}</div>
           </div>
         </div>
         <Button className="mt-4 w-full">
