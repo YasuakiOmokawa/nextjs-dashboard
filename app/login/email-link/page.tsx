f2ecc4abf7ba7d289858dcb021e6cec231c494ba
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-02-23 19:53:12 +0900

    use suspense to use searchParams

--- app/login/email-link/page.tsx
+++ app/login/email-link/page.tsx
@@ -1,10 +1,13 @@
 import LoginForm from "@/app/ui/email-link/login-form";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400]px flex-col space-y-2.5 p-4 md:-mt-32">
-        <LoginForm />
+        <Suspense>
+          <LoginForm />
+        </Suspense>
       </div>
     </main>
   );
