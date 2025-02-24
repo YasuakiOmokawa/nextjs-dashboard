8a438a2cc44d7bcf579a342c8dbe3317b666410a
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-02-24 11:22:43 +0900

    add IsString

--- lib/sample/utility-types-usage.ts
+++ lib/sample/utility-types-usage.ts
@@ -1,3 +1,7 @@
 import type { Assert, Equals } from "./utility-types";
 
 type _T01 = Assert<Equals<string, string>>;
+
+type IsString<T> = T extends string ? true : false;
+
+type _T02 = Assert<IsString<string>>;
