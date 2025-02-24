62521f4b6b6a2688f8fb505bfc4fdcd61b1281dc
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-02-24 11:36:10 +0900

    distributive conditional types

--- lib/sample/utility-types-usage.ts
+++ lib/sample/utility-types-usage.ts
@@ -5,3 +5,12 @@ type _T01 = Assert<Equals<string, string>>;
 type IsString<T> = T extends string ? true : false;
 
 type _T02 = Assert<IsString<string>>;
+
+type R1 = IsString<string | number>;
+type _T03 = Assert<Equals<R1, boolean>>;
+
+type _R2 = IsString<string> | IsString<number>;
+
+type _R3 =
+  | (string extends string ? true : false)
+  | (number extends string ? true : false);
