2e783386aebfe6d1d0077a440c5ed92b21fed3c9
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-03-09 17:14:00 +0900

    move old file

--- app/ui/profile/delete-account-button.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { Button } from "../button";
-
-export default function DeleteAccountButton() {
-  const handleClick = () => alert("本当によろしいですか？");
-
-  return <Button onClick={handleClick}>delete your account</Button>;
-}
