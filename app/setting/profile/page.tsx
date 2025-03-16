112348e0f0cfb84269748f1a4b99326131e0338c
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-03-16 16:13:10 +0900

    apply user profile form

--- app/setting/profile/page.tsx
+++ app/setting/profile/page.tsx
@@ -2,7 +2,7 @@ import { EditForm } from "@/app/ui/profile/edit-form";
 
 export default async function Page() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4">
+    <div className="flex flex-col p-4">
       <EditForm />
     </div>
   );
--- app/ui/profile/edit-form.tsx
+++ app/ui/profile/edit-form.tsx
@@ -43,70 +43,68 @@ export function EditForm() {
   });
 
   return (
-    <div className="flex-1 lg:max-w-2xl">
-      <div className="space-y-6">
-        <div>
-          <h3 className="text-lg font-medium">Profile</h3>
-          <p className="text-sm text-muted-foreground">
-            This is how others will see you on the site.
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Profile</h3>
+        <p className="text-sm text-muted-foreground">
+          This is how others will see you on the site.
+        </p>
+      </div>
+      <Separator />
+      <form
+        className="space-y-8"
+        id={form.id}
+        onSubmit={form.onSubmit}
+        action={action}
+        noValidate
+      >
+        <div className="space-y-2">
+          <Label htmlFor="name">Name</Label>
+          <Input
+            type="text"
+            key={fields.name.key}
+            name={fields.name.name}
+            defaultValue={fields.name.value ?? String(session?.user?.name)}
+            placeholder="John Doe"
+          />
+          <p className="text-xs text-muted-foreground">
+            This is your public display name.
+          </p>
+          <div className="text-red-500">{fields.name.errors}</div>
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Select>
+            <SelectTrigger id="email">
+              <SelectValue placeholder="Select a verified email to display" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="m@example.com">m@example.com</SelectItem>
+              <SelectItem value="m@google.com">m@google.com</SelectItem>
+              <SelectItem value="m@support.com">m@support.com</SelectItem>
+            </SelectContent>
+          </Select>
+          <p className="text-xs text-muted-foreground">
+            You can manage verified email addresses in your email settings.
           </p>
         </div>
-        <Separator />
-        <form
-          className="space-y-8"
-          id={form.id}
-          onSubmit={form.onSubmit}
-          action={action}
-          noValidate
-        >
-          <div className="space-y-2">
-            <Label htmlFor="name">Name</Label>
-            <Input
-              type="text"
-              key={fields.name.key}
-              name={fields.name.name}
-              defaultValue={fields.name.value ?? String(session?.user?.name)}
-              placeholder="John Doe"
-            />
-            <p className="text-xs text-muted-foreground">
-              This is your public display name.
-            </p>
-            <div className="text-red-500">{fields.name.errors}</div>
-          </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Select>
-              <SelectTrigger id="email">
-                <SelectValue placeholder="Select a verified email to display" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="m@example.com">m@example.com</SelectItem>
-                <SelectItem value="m@google.com">m@google.com</SelectItem>
-                <SelectItem value="m@support.com">m@support.com</SelectItem>
-              </SelectContent>
-            </Select>
-            <p className="text-xs text-muted-foreground">
-              You can manage verified email addresses in your email settings.
-            </p>
-          </div>
+        <div className="space-y-2">
+          <Label htmlFor="bio">Bio</Label>
+          <Textarea
+            id="bio"
+            placeholder="Tell us a little bit about yourself"
+            className="min-h-[100px]"
+            defaultValue="I own a computer."
+          />
+          <p className="text-xs text-muted-foreground">
+            You can @mention other users and organizations to link to them.
+          </p>
+        </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="bio">Bio</Label>
-            <Textarea
-              id="bio"
-              placeholder="Tell us a little bit about yourself"
-              className="min-h-[100px]"
-              defaultValue="I own a computer."
-            />
-            <p className="text-xs text-muted-foreground">
-              You can @mention other users and organizations to link to them.
-            </p>
-          </div>
-
-          <Button type="submit">Update</Button>
-        </form>
-      </div>
+        <Button type="submit">Update</Button>
+      </form>
     </div>
   );
 }
