79c1e9c0eb328a9c6ef4e38f963b80f94b31d0f6
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-03-16 15:53:45 +0900

    add profile page

--- app/setting/dashboard/page.tsx
+++ app/setting/dashboard/page.tsx
@@ -1,12 +1,9 @@
+import { EditForm } from "@/app/ui/profile/edit-form";
+
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-      </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      <EditForm />
     </div>
   );
 }
--- app/setting/profile/page.tsx
+++ app/setting/profile/page.tsx
@@ -1,5 +1,9 @@
 import { EditForm } from "@/app/ui/profile/edit-form";
 
 export default async function Page() {
-  return <EditForm />;
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4">
+      <EditForm />
+    </div>
+  );
 }
