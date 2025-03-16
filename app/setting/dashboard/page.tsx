b2414c977ca0ac156c91b8f6edad1ee17e7857c2
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-03-16 15:55:16 +0900

    remove setting dashboard page

--- app/setting/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EditForm } from "@/app/ui/profile/edit-form";
-
-export default function Page() {
-  return (
-    <div className="flex flex-1 flex-col gap-4 p-4">
-      <EditForm />
-    </div>
-  );
-}
