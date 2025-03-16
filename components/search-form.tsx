df261faf2c4001ef5c7c9913772f23db364d7885
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-03-16 15:41:50 +0900

    add setting dashboard

---
+++ components/search-form.tsx
@@ -0,0 +1,22 @@
+import { Search } from "lucide-react"
+
+import { Label } from "@/components/ui/label"
+import { SidebarInput } from "@/components/ui/sidebar"
+
+export function SearchForm({ ...props }: React.ComponentProps<"form">) {
+  return (
+    <form {...props}>
+      <div className="relative">
+        <Label htmlFor="search" className="sr-only">
+          Search
+        </Label>
+        <SidebarInput
+          id="search"
+          placeholder="Type to search..."
+          className="h-8 pl-7"
+        />
+        <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
+      </div>
+    </form>
+  )
+}
