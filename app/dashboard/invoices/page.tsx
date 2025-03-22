181c7aefd2fc01041f1ecf06f153c0ecda16b111
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-03-22 22:02:33 +0900

    タイトル日本語化

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -20,7 +20,7 @@ export default async function Page(props: {
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+        <h1 className={`${lusitana.className} text-2xl`}>請求書一覧</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search Invoices..." />
